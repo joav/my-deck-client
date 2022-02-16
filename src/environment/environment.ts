33f09bd4a6d9e213531236268af634d96c33bc39
joav <jim.acero.valcarcel@gmail.com>
2022-02-16 09:37:54 -0500

    Receive board update from socket

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  api: 'http://localhost:3000/api/'
+  api: 'http://localhost:3000/api/',
+  socket: 'ws://localhost:3000',
 };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
+import { io } from "socket.io-client";
 import { environment } from "./environment/environment";
 import "./scss/styles.scss";
 
 window.addEventListener('DOMContentLoaded', (event) => {
+  const socket = io(environment.socket);
+
+  socket.on('board', function(b) {
+    console.log(b)    
+  });
 });
 
